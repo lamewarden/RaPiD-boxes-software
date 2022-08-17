8a38704755033177655170b70ba46466905a4711
lamewarden <kashkan.van@gmail.com>
2022-08-17 14:34:02 +0200

    Box is in the "original" state (pre-fail)

---
+++ GUI/meta_data.py
@@ -0,0 +1,7 @@
+# Experiment info
+total_experiment_length=110
+apical_decision=1
+apical_hours=90
+ph_decision=1
+light=[0, 254, 254, 0]
+location='/home/pi/camera/Experiments/RUYT/2022.08.17_PI3__HYY'
