e12126381a97294d16630d52ab7f1f41d719e6e1
lamewarden <kashkan.van@gmail.com>
2022-08-18 15:28:17 +0200

    killer buttons are added and are functional (should be adjusted)

--- GUI/meta_data.py
+++ GUI/meta_data.py
@@ -1,7 +1,10 @@
 # Experiment info
 total_experiment_length=110
+prelight_decision=0
 apical_decision=1
 apical_hours=90
 ph_decision=1
-light=[0, 254, 254, 0]
-location='/home/pi/camera/Experiments/RUYT/2022.08.17_PI3__HYY'
+light=[0, 10, 10, 0]
+location='/home/pi/camera/Experiments/FFFFFUT/2022.08.18_PI3__HHHHYT'
+main_process_PID=1654
+status_process_PID=1678
