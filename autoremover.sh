dbb1230b7c511ac831d8670f2eb38c66742cb5f9
Ivan Kashkan <kashkan.van@gmail.com>
2025-07-08 19:47:42 +0200

    Update autoremover.sh

--- autoremover.sh
+++ autoremover.sh
@@ -4,4 +4,4 @@
 dir="/home/pi/camera/Experiments"
 
 # Use the find command to find files and directories older than two months and delete them
-find "$dir" -mtime +60 -exec rm -rf {} \;
+find "$dir" -mindepth 1 -mtime +60 -exec rm -rf {} \;
